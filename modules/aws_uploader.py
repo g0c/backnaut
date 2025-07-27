5272c5080a48c7323314b6f8221c7bdc9f411542
g0c <gooooc@gmail.com>
2025-07-27 16:15:43 +0200

    aws changes

--- modules/aws_uploader.py
+++ modules/aws_uploader.py
@@ -1,7 +1,12 @@
 import boto3
 
+session = boto3.Session(profile_name='myprofile')
+s3 = session.client('s3')
+
 def upload_to_s3(file_path, bucket, access_key, secret_key, region):
     s3 = boto3.client('s3', region_name=region,
                       aws_access_key_id=access_key,
                       aws_secret_access_key=secret_key)
     s3.upload_file(file_path, bucket, f"backups/{file_path}")
+
+
